d79bc6f634fdc7c3b95d0c7b93fec3b2024de24e
xaim8870 <usamajabar.2@gmail.com>
2025-09-29 20:54:52 +0500

    Updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 // src/App.tsx
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ThemeProvider  from './contexts/ThemeContext';
+
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -18,7 +18,7 @@ function App() {
   
 
   return (
-    <ThemeProvider>
+  
       <Router>
         <div className="flex flex-col min-h-screen">
           <Navbar />
@@ -39,7 +39,7 @@ function App() {
           <Footer />
         </div>
       </Router>
-    </ThemeProvider>
+    
   );
 }
 
